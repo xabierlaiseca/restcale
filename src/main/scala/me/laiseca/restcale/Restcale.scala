fcdb2b2f2e5ed4dea09fef6e270ba5a01d224caf
xabierlaiseca <xabier@laiseca.me>
2013-09-14 00:05:51 +0100

    Main object renamed to Restcale

--- src/main/scala/me/laiseca/restcale/Restcale.scala
+++ src/main/scala/me/laiseca/restcale/Restcale.scala
@@ -2,7 +2,7 @@ package me.laiseca.restcale
 
 import me.laiseca.restcale.http.server.HttpServer
 
-object Main {
+object Restcale {
 
   def main(args: Array[String]): Unit = {
     new HttpServer().run()
