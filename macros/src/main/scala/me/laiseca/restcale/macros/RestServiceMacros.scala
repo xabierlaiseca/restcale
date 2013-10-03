df57537bfdf8825473a134264cf5dcfe53cbd76e
xabierlaiseca <xabier@laiseca.me>
2013-10-03 23:57:53 +0100

    Checked duplicated urls in macro

--- macros/src/main/scala/me/laiseca/restcale/macros/RestServiceMacros.scala
+++ macros/src/main/scala/me/laiseca/restcale/macros/RestServiceMacros.scala
@@ -4,8 +4,18 @@ import scala.collection.immutable.List
 import scala.reflect.macros.Context
 import me.laiseca.restcale.internal.function.BaseRestFunction
 import me.laiseca.restcale.http.HttpMethod
+import scala.collection.mutable.HashSet
+import scala.collection.mutable.Set
+import scala.collection.immutable.HashMap
 
 object RestServiceMacros {
+  
+  private val pathPatternsByMethod = HashMap[String, Set[String]](
+      HttpMethod.DELETE -> HashSet(),
+      HttpMethod.GET -> HashSet(),
+      HttpMethod.PATCH -> HashSet(),
+      HttpMethod.POST -> HashSet(),
+      HttpMethod.PUT -> HashSet())
   
   val BASE_CLASS_NAME = classOf[BaseRestFunction].getPackage().getName() + ".RestFunction"
   
@@ -33,12 +43,23 @@ object RestServiceMacros {
   private def buildFunction[R](c: Context)(f:c.Expr[R], path:c.Expr[String], method: String) = {
     import c.universe._
     
-    val (thisType, args) = f.tree.tpe match {
-      case TypeRef(thisType, _, args) => (thisType, args)
+    def validateUrl(pathExpr:c.Expr[String], method:String) = {
+      val Literal(Constant(path:String)) = pathExpr.tree
+      val pathPatterns = pathPatternsByMethod.getOrElse(method, null)
+      val paramPattern = ":[^/]+".r
+      val pathPattern = paramPattern.replaceAllIn(path, "<wildcard>")
+      
+      if(pathPatterns contains pathPattern) {
+        c.abort(c.enclosingPosition, "url pattern already defined")
+      } else {
+        pathPatterns add pathPattern
+      }
     }
     
-    val clazz = c.mirror.staticClass(BASE_CLASS_NAME + (args.size - 1) )
+    validateUrl(path, method)
     
+    val TypeRef(thisType, _, args) = f.tree.tpe
+    val clazz = c.mirror.staticClass(BASE_CLASS_NAME + (args.size - 1) )
     val clazzType = TypeRef(thisType, clazz , args)
     
     c.Expr[BaseRestFunction](Apply(
--- macros/src/test/scala/me/laiseca/restcale/macros/RestServiceMacrosSpec.scala
+++ macros/src/test/scala/me/laiseca/restcale/macros/RestServiceMacrosSpec.scala
@@ -38,225 +38,225 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   private def put[A](path:String)(f: => A) = macro RestServiceMacros.put[A]
   
   "the macro with 0 parameters function" should "create a RestFunction0 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { () =>
+    def method = get("/path0") { () =>
       new R()
     }
     
     assertResult(classOf[RestFunction0[_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path0")(method.path)
     assertResult(List(classOf[R]))(argsInfo(method))
   }
   
   "the macro with 1 parameter function" should "create a RestFunction1 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1) =>
+    def method = get("/path1") { (t1:T1) =>
       new R()
     }
     
     assertResult(classOf[RestFunction1[_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path1")(method.path)
     assertResult(List(classOf[T1], classOf[R]))(argsInfo(method))
   }
     
   "the macro with 2 parameters function" should "create a RestFunction2 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2) =>
+    def method = get("/path2") { (t1:T1, t2:T2) =>
       new R()
     }
     
     assertResult(classOf[RestFunction2[_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path2")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 3 parameters function" should "create a RestFunction3 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3) =>
+    def method = get("/path3") { (t1:T1, t2:T2, t3:T3) =>
       new R()
     }
     
     assertResult(classOf[RestFunction3[_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path3")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 4 parameters function" should "create a RestFunction4 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4) =>
+    def method = get("/path4") { (t1:T1, t2:T2, t3:T3, t4:T4) =>
       new R()
     }
     
     assertResult(classOf[RestFunction4[_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path4")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 5 parameters function" should "create a RestFunction5 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5) =>
+    def method = get("/path5") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5) =>
       new R()
     }
     
     assertResult(classOf[RestFunction5[_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path5")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[R]))(
         argsInfo(method))
   }
   
   "the macro with 6 parameters function" should "create a RestFunction6 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6) =>
+    def method = get("/path6") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6) =>
       new R()
     }
     
     assertResult(classOf[RestFunction6[_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path6")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[R]))(argsInfo(method))
   }
   
   "the macro with 7 parameters function" should "create a RestFunction7 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path7") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7) =>
       new R()
     }
     
     assertResult(classOf[RestFunction7[_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path7")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 8 parameters function" should "create a RestFunction8 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path8") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8) =>
       new R()
     }
     
     assertResult(classOf[RestFunction8[_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path8")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 9 parameters function" should "create a RestFunction9 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path9") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9) =>
       new R()
     }
     
     assertResult(classOf[RestFunction9[_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path9")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 10 parameters function" should "create a RestFunction10 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path10") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10) =>
       new R()
     }
     
     assertResult(classOf[RestFunction10[_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path10")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 11 parameters function" should "create a RestFunction11 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path11") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11) =>
       new R()
     }
     
     assertResult(classOf[RestFunction11[_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path11")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 12 parameters function" should "create a RestFunction12 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path12") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12) =>
       new R()
     }
     
     assertResult(classOf[RestFunction12[_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path12")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[R]))(
         argsInfo(method))
   }
   
   "the macro with 13 parameters function" should "create a RestFunction13 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path13") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13) =>
       new R()
     }
     
     assertResult(classOf[RestFunction13[_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path13")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[R]))(argsInfo(method))
   }
   
   "the macro with 14 parameters function" should "create a RestFunction14 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path14") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14) =>
       new R()
     }
     
     assertResult(classOf[RestFunction14[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path14")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[R]))(argsInfo(method))
   }
 
   "the macro with 15 parameters function" should "create a RestFunction15 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path15") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15) =>
       new R()
     }
     
     assertResult(classOf[RestFunction15[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path15")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 16 parameters function" should "create a RestFunction16 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path16") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16) =>
       new R()
     }
     
     assertResult(classOf[RestFunction16[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path16")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 17 parameters function" should "create a RestFunction17 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path17") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16,
         t17:T17) =>
       new R()
     }
     
     assertResult(classOf[RestFunction17[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path17")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[T17], classOf[R]))(argsInfo(method))
   }
   
   "the macro with 18 parameters function" should "create a RestFunction18 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path18") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16,
         t17:T17, t18:T18) =>
       new R()
     }
     
     assertResult(classOf[RestFunction18[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path18")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[T17], classOf[T18], classOf[R]))(
@@ -264,14 +264,14 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the macro with 19 parameters function" should "create a RestFunction19 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path19") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16,
         t17:T17, t18:T18, t19:T19) =>
       new R()
     }
     
     assertResult(classOf[RestFunction19[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path19")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[T17], classOf[T18], classOf[T19], 
@@ -279,7 +279,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the macro with 20 parameters function" should "create a RestFunction20 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path20") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16,
         t17:T17, t18:T18, t19:T19, t20:T20) =>
       new R()
@@ -287,7 +287,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
     
     assertResult(classOf[RestFunction20[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(
         method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path20")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[T17], classOf[T18], classOf[T19], 
@@ -295,7 +295,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the macro with 21 parameters function" should "create a RestFunction21 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path21") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16,
         t17:T17, t18:T18, t19:T19, t20:T20, t21:T21) =>
       new R()
@@ -303,7 +303,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
     
     assertResult(classOf[RestFunction21[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(
         method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path21")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[T17], classOf[T18], classOf[T19], 
@@ -311,7 +311,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the macro with 22 parameters function" should "create a RestFunction22 with the given data" in {
-    def method = get(RestServiceMacrosSpec.PATH) { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
+    def method = get("/path22") { (t1:T1, t2:T2, t3:T3, t4:T4, t5:T5, t6:T6,
         t7:T7, t8:T8, t9:T9, t10:T10, t11:T11, t12:T12, t13:T13, t14:T14, t15:T15, t16:T16,
         t17:T17, t18:T18, t19:T19, t20:T20, t21:T21, t22:T22) =>
       new R()
@@ -319,7 +319,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
     
     assertResult(classOf[RestFunction22[_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]])(
         method.getClass())
-    assertResult(RestServiceMacrosSpec.PATH)(method.path)
+    assertResult("/path22")(method.path)
     assertResult(List(classOf[T1], classOf[T2], classOf[T3], classOf[T4], classOf[T5], classOf[T6],
         classOf[T7], classOf[T8], classOf[T9], classOf[T10], classOf[T11], classOf[T12], classOf[T13],
         classOf[T14], classOf[T15], classOf[T16], classOf[T17], classOf[T18], classOf[T19], 
@@ -327,7 +327,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the DELETE method macro" should "return DELETE as HTTP method" in {
-    def method = delete(RestServiceMacrosSpec.PATH) { () =>
+    def method = delete("/path") { () =>
       new R()
     }
     
@@ -335,7 +335,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the GET method macro" should "return GET as HTTP method" in {
-    def method = get(RestServiceMacrosSpec.PATH) { () =>
+    def method = get("/path") { () =>
       new R()
     }
     
@@ -343,7 +343,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the PATCH method macro" should "return PATCH as HTTP method" in {
-    def method = patch(RestServiceMacrosSpec.PATH) { () =>
+    def method = patch("/path") { () =>
       new R()
     }
     
@@ -351,7 +351,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the POST method macro" should "return POST as HTTP method" in {
-    def method = post(RestServiceMacrosSpec.PATH) { () =>
+    def method = post("/path") { () =>
       new R()
     }
     
@@ -359,7 +359,7 @@ class RestServiceMacrosSpec extends FlatSpec with Matchers {
   }
   
   "the PUT method macro" should "return PUT as HTTP method" in {
-    def method = put(RestServiceMacrosSpec.PATH) { () =>
+    def method = put("/path") { () =>
       new R()
     }
     
@@ -380,7 +380,3 @@ class T11; class T12; class T13; class T14; class T15; class T16; class T17; cla
 class T21; class T22
 
 class R
-
-object RestServiceMacrosSpec {
-  val PATH = "/path"
-}
