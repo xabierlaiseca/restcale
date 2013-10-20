9b0bed7ab2a9d4a3d6de380a5eb9f37ef214bb49
xabierlaiseca <xabier@laiseca.me>
2013-10-20 19:09:22 +0100

    HttpRequest is immutable

--- core/src/main/scala/me/laiseca/restcale/http/HttpRequest.scala
+++ core/src/main/scala/me/laiseca/restcale/http/HttpRequest.scala
@@ -1,12 +1,22 @@
 package me.laiseca.restcale.http
 
+import scala.collection.JavaConversions._
 import io.netty.handler.codec.http.QueryStringDecoder
+import io.netty.handler.codec.http.HttpHeaders
 
 class HttpRequest(request: io.netty.handler.codec.http.HttpRequest) {
   private val decoder = new QueryStringDecoder(request.getUri())
     
-  def headers = request.headers
-  def method = request.getMethod.name.toUpperCase
-  def path = decoder.path
-  def parameters = decoder.parameters
+  val headers = toImmutableHeaders(request.headers)
+  val method = request.getMethod.name.toUpperCase
+  val path = decoder.path
+  val parameters = toImmutableParameters(decoder.parameters)
+  
+  private def toImmutableHeaders(headers: HttpHeaders) = {
+    headers.map(entry => (entry.getKey(), entry.getValue())).toMap
+  }
+  
+  private def toImmutableParameters(parameters: java.util.Map[String, java.util.List[String]]) = {
+    parameters.map(entry => (entry._1, entry._2.toList)).toMap
+  }
 }
