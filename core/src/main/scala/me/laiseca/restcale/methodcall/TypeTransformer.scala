3b3b94ed50aeb2f24dd63a9f85825dd2b4d5edfe
xabierlaiseca <xabier@laiseca.me>
2013-10-21 20:15:27 +0100

    Support for transforming strings given a runtime type in TypeTransformer

--- core/src/main/scala/me/laiseca/restcale/methodcall/TypeTransformer.scala
+++ core/src/main/scala/me/laiseca/restcale/methodcall/TypeTransformer.scala
@@ -4,43 +4,48 @@ import scala.reflect.runtime.{universe => ru}
 import scala.reflect.runtime.universe._
 
 class TypeTransformer {
-  def supports[T](implicit ttag: TypeTag[T]) = {
-    TypeTransformer.TYPE_TRANSFORMERS.contains(ttag)
-  }
+  def supports[T](implicit ttag: TypeTag[T]):Boolean = supports(ttag.tpe)
   
-  def transform[T: ru.TypeTag](stringValue: String)(implicit ttag:TypeTag[T]):Option[T] = {
-    val transformer = TypeTransformer.TYPE_TRANSFORMERS.get(ttag)
+  def supports(tpe: Type):Boolean = TypeTransformer.TYPE_TRANSFORMERS.contains(tpe)
+  
+  def transform[T: ru.TypeTag](stringValue: String)(implicit ttag:TypeTag[T]):Option[T] =
+    Option.apply(transformValue(stringValue, ttag.tpe).asInstanceOf[T])
+    
+  def transform(stringValue: String, tpe: Type):Option[Any] =
+    Option.apply(transformValue(stringValue, tpe))
+    
+  private def transformValue(stringValue: String, tpe: Type):Any = {
+    val transformer = TypeTransformer.TYPE_TRANSFORMERS.get(tpe)
     
     if(transformer.isDefined) {
       try {
-        val value = (transformer.get)(stringValue)
-        Option.apply(value.asInstanceOf[T])
+        (transformer.get)(stringValue)
       } catch {
         case e@(_:IllegalArgumentException | _:NumberFormatException)
         	=> throw new IllegalValueException(stringValue, e)
       }
     } else {
-      Option.empty
+      null
     }
   }
 }
 
 private object TypeTransformer {
-  val TYPE_TRANSFORMERS:Map[TypeTag[_],(String) => Any] = Map(
-    ru.typeTag[Byte] -> ((value:String) => value.toByte),
-    ru.typeTag[Short] -> ((value:String) => value.toShort),
-    ru.typeTag[Int] -> ((value:String) => value.toInt),
-    ru.typeTag[Long] -> ((value:String) => value.toLong),
-    ru.typeTag[Float] -> ((value:String) => value.toFloat),
-    ru.typeTag[Double] -> ((value:String) => value.toDouble),
-    ru.typeTag[Char] -> ((value:String) => {
+  val TYPE_TRANSFORMERS:Map[Type,(String) => Any] = Map(
+    ru.typeTag[Byte].tpe -> ((value:String) => value.toByte),
+    ru.typeTag[Short].tpe -> ((value:String) => value.toShort),
+    ru.typeTag[Int].tpe -> ((value:String) => value.toInt),
+    ru.typeTag[Long].tpe -> ((value:String) => value.toLong),
+    ru.typeTag[Float].tpe -> ((value:String) => value.toFloat),
+    ru.typeTag[Double].tpe -> ((value:String) => value.toDouble),
+    ru.typeTag[Char].tpe -> ((value:String) => {
       if(value.length() == 1) {
     	  value.charAt(0)
       } else {
         throw new IllegalArgumentException
       }
     }),
-    ru.typeTag[String] -> ((value:String) => value),
-    ru.typeTag[Boolean] -> ((value:String) => value.toBoolean)
+    ru.typeTag[String].tpe -> ((value:String) => value),
+    ru.typeTag[Boolean].tpe -> ((value:String) => value.toBoolean)
   )
 }
--- core/src/test/scala/me/laiseca/restcale/methodcall/TypeTransformerSpec.scala
+++ core/src/test/scala/me/laiseca/restcale/methodcall/TypeTransformerSpec.scala
@@ -1,5 +1,6 @@
 package me.laiseca.restcale.methodcall
 
+import scala.reflect.runtime.{universe => ru}
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 
@@ -13,11 +14,35 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     }
   }
   
+  "given a supported runtime type, supports" should "return true" in {
+    val testObj = new TypeTransformer
+    
+    assert{
+      testObj.supports(ru.typeOf[Int])
+    }
+  }
+  
   "given a not supported type, supports" should "return false" in {
     val testObj = new TypeTransformer
     
     assertResult(false){
       testObj.supports[NotSupportedType]
+    }
+  }
+  
+  "given a not supported runtime type, supports" should "return false" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(false){
+      testObj.supports(ru.typeOf[NotSupportedType])
+    }
+  }
+  
+  "given a String containing a Byte, transform with runtime type" should "return the Byte value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(10.toByte) {
+      testObj.transform("10", ru.typeOf[Byte]).get
     }
   }
   
@@ -45,6 +70,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     }
   }
   
+  "given a String containing a Short, transform with runtime type" should "return the Short value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(10.toShort) {
+      testObj.transform("10", ru.typeOf[Short]).get
+    }
+  }
+  
   "given a String not containing a Short, transform to Short" should "throw an exception" in {
     val testObj = new TypeTransformer
 
@@ -58,6 +91,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     
     assertResult(10) {
       testObj.transform[Int]("10").get
+    }
+  }
+  
+   "given a String containing a Int, transform with runtime type" should "return the Int value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(10) {
+      testObj.transform("10", ru.typeOf[Int]).get
     }
   }
   
@@ -77,6 +118,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     }
   }
   
+  "given a String containing a Long, transform with runtime type" should "return the Long value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(10.toLong) {
+      testObj.transform("10", ru.typeOf[Long]).get
+    }
+  }
+  
   "given a String not containing a Long, transform to Long" should "throw an exception" in {
     val testObj = new TypeTransformer
 
@@ -90,6 +139,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     
     assertResult(10.2.toFloat) {
       testObj.transform[Float]("10.2").get
+    }
+  }
+  
+  "given a String containing a Float, transform with runtime type" should "return the Float value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(10.2.toFloat) {
+      testObj.transform("10.2", ru.typeOf[Float]).get
     }
   }
   
@@ -109,6 +166,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     }
   }
   
+  "given a String containing a Double, transform with runtime type" should "return the Double value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(10.2) {
+      testObj.transform("10.2", ru.typeOf[Double]).get
+    }
+  }
+  
   "given a String not containing a Double, transform to Double" should "throw an exception" in {
     val testObj = new TypeTransformer
 
@@ -122,6 +187,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     
     assertResult('c') {
       testObj.transform[Char]("c").get
+    }
+  }
+  
+  "given a String containing a Char, transform with runtime type" should "return the Char value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult('c') {
+      testObj.transform("c", ru.typeOf[Char]).get
     }
   }
   
@@ -141,6 +214,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     }
   }
   
+  "given a String containing a Boolean, transform with runtime type" should "return the Boolean value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult(true) {
+      testObj.transform("true", ru.typeOf[Boolean]).get
+    }
+  }
+  
   "given a String not containing a Boolean, transform to Boolean" should "throw an exception" in {
     val testObj = new TypeTransformer
 
@@ -154,6 +235,14 @@ class TypeTransformerSpec extends FlatSpec with Matchers {
     
     assertResult("something") {
       testObj.transform[String]("something").get
+    }
+  }
+  
+  "given a String, transform to String with runtime type" should "return the same value" in {
+    val testObj = new TypeTransformer
+    
+    assertResult("something") {
+      testObj.transform("something", ru.typeOf[String]).get
     }
   }
   
